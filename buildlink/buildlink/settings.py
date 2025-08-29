733b3a1e689a12d2a75e905ab4148f01654562f2
Nkejumuremyi <nkejumuremyidavid@gmail.com>
2025-08-29 10:04:44 +0200

    models file

--- buildlink/buildlink/settings.py
+++ buildlink/buildlink/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'buildlink.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'buildlink_db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
